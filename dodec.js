1fd30a82744efa3361c9658988e6ffc48a53ae42
StringTheery <jmbarr@etczone.com>
2014-08-29 07:25:15 -0400

    deglobalize variables

--- dodec.js
+++ dodec.js
@@ -1,15 +1,15 @@
 var farX = 700;
 var lowY = 700;
-var c=document.getElementById("can1");
-var ctx=c.getContext("2d");
-var c2=document.getElementById("can2");
-var ctx2=c2.getContext("2d");
+
+
 $(document).ready(function(){
 background();
 hill();
 
 
 function hill(){
+	var c2=document.getElementById("can2");
+	var ctx2=c2.getContext("2d");
 	ctx2.strokeStyle="#E5E4E2";
 	ctx2.beginPath();
 	ctx2.moveTo(farX, lowY/2)
@@ -18,6 +18,8 @@ function hill(){
 }
 //draw background
 function background(){
+	var c=document.getElementById("can1");
+	var ctx=c.getContext("2d");
 	ctx.fillStyle="#151B54";
 	ctx.fillRect(0,0,700,700);
 
