cdcdbc70baff62d00c8ea76d932788725a1a42ae
StringTheery <jmbarr@etczone.com>
2014-08-30 00:21:10 -0400

    Daily Build 8/29/14

--- dodec.js
+++ dodec.js
@@ -1,14 +1,30 @@
-var farX = 700;
-var lowY = 700;
+var height = $(window).height();
+var width = $(window).width();
+var farX = width;
+var lowY = height;
+var scale = 25;
+
 
 $(document).ready(function(){
 background();
 hill();
+dodec();
+document.addEventListener('keydown',function(event){
+	if(event.keyCode == 38){
+		$("#can3").animate({top:"-60px"}).animate({top:"127px"});
+	}
+});
+
+
+
+
 
 //draw hill down which boulder will roll
 function hill(){
 	var c2=document.getElementById("can2");
 	var ctx2=c2.getContext("2d");
+	c2.setAttribute("height",height);
+	c2.setAttribute("width",width);
 	ctx2.strokeStyle="#E5E4E2";
 	ctx2.beginPath();
 	ctx2.moveTo(farX, lowY/2)
@@ -19,9 +35,38 @@ function hill(){
 function background(){
 	var c=document.getElementById("can1");
 	var ctx=c.getContext("2d");
+	c.setAttribute("height",height);
+	c.setAttribute("width",width);
 	ctx.fillStyle="#151B54";
 	ctx.fillRect(0,0,farX,lowY);
 
 }
+//draw a dodecagon
+function dodec(){
+	var move=0;
+	var c3=document.getElementById("can3");
+	c3.setAttribute("height",12*scale);
+	c3.setAttribute("width",12*scale);
+	var ct=c3.getContext("2d");
+	ct.strokeStyle="#E5E4E2";
+	ct.beginPath();
+	ct.moveTo(12*scale,6*scale+move);
+	ct.lineTo(11*scale,9*scale+move);
+	ct.lineTo(9*scale,11*scale+move);
+	ct.lineTo(6*scale,12*scale+move);
+	ct.lineTo(3*scale,11*scale+move);
+	ct.lineTo(1*scale,9*scale+move);
+	ct.lineTo(0*scale,6*scale+move);
+	ct.lineTo(1*scale,3*scale+move);
+	ct.lineTo(3*scale,1*scale+move);
+	ct.lineTo(6*scale,0*scale+move);
+	ct.lineTo(9*scale,1*scale+move);
+	ct.lineTo(11*scale,3*scale+move);
+	ct.closePath();
+	ct.stroke();
+	move+=1;
+
+}
 
 });
+
