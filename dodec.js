7a1a9f9c1cf412135b680a7cae769a1e2c312b44
kWil6 <kjwilson_1@hotmail.com>
2014-09-18 11:25:45 -0400

    Update dodec.js

--- dodec.js
+++ dodec.js
@@ -124,7 +124,7 @@ function obs(){
     	}
 	ct.strokeRect(obsX,obsY,obsHeight,obsWidth);
 	obsX += .0025*width*obsChange;
-	obsY -= .0017*height*obsChange;
+	obsY -= .0016*height*obsChange;
 	if (obsX>farX){
 		if(boopmeter == 0){
 			score += 1;
