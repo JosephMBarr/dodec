8a24c1d2ee424c8409b4184c041ce0acdc467cd5
kWil6 <kjwilson_1@hotmail.com>
2014-09-22 18:26:41 -0400

    Update dodec.js

--- dodec.js
+++ dodec.js
@@ -149,7 +149,12 @@ function obs(){
 }
 
 function splash(){
-        lives=3
+        move = 0;
+        change = 0;
+        score = 0;
+        lives = 3;
+        obsChange = 1;
+        randobs = 1;
 	var splashCanvas=document.getElementById("splash");
 	splashCanvas.setAttribute("height",height);
 	splashCanvas.setAttribute("width",width);
