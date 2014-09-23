594a5fd896c781ba728dd0e81804255b5234b063
kWil6 <kjwilson_1@hotmail.com>
2014-09-22 22:01:04 -0400

    Update dodec.js

--- dodec.js
+++ dodec.js
@@ -127,7 +127,7 @@ function obs(){
 	ct.fillRect(obsX,obsY,obsHeight,obsWidth);
 	obsX += width*obsChange/400;
 	obsY -= height*obsChange/800;
-        if(ct.isPointInPath(obsX+obsWidth,obsY)){
+        if(ct.isPointInPath(obsX+obsWidth,obsY-obsHeight)){
 		if(obsChange>1){
  		        obsChange-=1;
 		}
