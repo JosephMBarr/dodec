7501163df755b92281bf7bdd1b7b5905b517c14a
StringTheery <jmbarr@etczone.com>
2014-10-13 11:06:34 -0400

    Update dodec.js

--- dodec.js
+++ dodec.js
@@ -185,7 +185,7 @@ function obs(){
     		obsWidth = Math.floor((Math.random() * 40) + 25);
     		obsHeight = Math.floor((Math.random() * 25) + 25);
     		obsWait = Math.floor(Math.random() * 100)
-    		obsX = 0-(obsWidth);
+    		obsX = 0;
 		obsY = height-obsHeight;
     	}
 	ct.fillRect(obsX,obsY,obsWidth,obsHeight);
