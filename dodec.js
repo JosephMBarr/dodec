2ddc61275e1db537f19f5cb921d9c9aade943ae8
Joey Barr <jmbarr@etczone.com>
2014-11-16 00:08:11 -0500

    asdf

--- dodec.js
+++ dodec.js
@@ -49,7 +49,7 @@
 	}
 	window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
 	window.webkitRequestAnimationFrame || window.oRequestAnimationFrame;
-if (window.location.protocol != "https:"){window.location.href = "https:" + window.location.href.substring(window.location.protocol.length)};
+//if (window.location.protocol != "https:"){window.location.href = "https:" + window.location.href.substring(window.location.protocol.length)};
 	$(document).ready(function(){
 	$(window).resize(function(){
 	    location.reload();
